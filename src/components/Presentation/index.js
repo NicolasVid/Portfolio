0c839c1754f83b5252c6311def063b3e15ce40ed
NicolasVid <65497850+NicolasVid@users.noreply.github.com>
2020-10-02 16:05:46 +0200

    Logo réseaux

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import './presentation.scss';
 import avatar from 'src/images/avatar.png';
+import githubLogo from 'src/images/github-logo.png';
+import linkedin from 'src/images/linkedin.png';
 
 const Presentation = () => (
   <div className="presentation">
+    <div className="presentation__images__container">
+      <a href="https://github.com/NicolasVid" className="presentation__github">
+        <img
+          src={githubLogo}
+          alt="Banner GIF"
+        />
+      </a>
+      <a href="https://www.linke•din.com/in/nicolasvidale/" className="presentation__linkedin">
+        <img
+          src={linkedin}
+          alt="Banner GIF"
+        />
+      </a>
+    </div>
     <h1>Nicolas Vidale</h1>
     <h2>Développeur Front-end passionné</h2>
     <img
       src={avatar}
-      className="presentation__image"
+      className="presentation__avatar"
       alt="Banner GIF"
     />
+    <div className="half-circle" />
   </div>
 );
 
